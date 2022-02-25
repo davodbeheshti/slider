4bdd29a19d7b112661eb51c94119bf1d23ad6a41
davodbeheshti <davood.beheshti2018@gmail.com>
2022-02-25 13:14:02 +0330

    create function append to element slider

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 // import Swiper from './swiper';
 const boxs = document.querySelectorAll('#box');
+const imgsBox1 = ['img1.png', 'img2.png', 'img3.png', 'img4.png', 'img5.png', 'img6.png']
+const imgsBox2 = ['img1.png', 'img2.png', 'img3.png', 'img4.png', 'img5.png', 'img6.png']
+const imgsBox3 = ['img1.png', 'img2.png', 'img3.png', 'img4.png', 'img5.png', 'img6.png']
+const imgsBox4 = ['img1.png', 'img2.png', 'img3.png', 'img4.png', 'img5.png', 'img6.png']
 
 boxs.forEach(x => {
     let classElement = x.className;
@@ -7,16 +11,24 @@ boxs.forEach(x => {
         switch (classElement) {
             case 'box-1':
                 console.log('box-1')
+                appendSwiper(imgsBox1)
                 break;
             case 'box-2':
                 console.log('box-2')
+                appendSwiper(imgsBox2)
                 break;
             case 'box-3':
                 console.log('box-3')
+                appendSwiper(imgsBox3)
                 break;
             case 'box-4':
                 console.log('box-4')
+                appendSwiper(imgsBox4)
                 break;
         }
     })
 })
+
+const appendSwiper = (arrayImgs) => {
+    console.log(arrayImgs)
+}
